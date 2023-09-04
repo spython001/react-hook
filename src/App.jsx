93125d4ac0bfa41f1126a0930209aa8edb27d9da
spython <emmanuelegbo01@gmail.com>
2023-09-04 15:53:59 +0100

    added a class counter UE file to demonstrate conditional run useEffect hook

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
-import ClassCounterOne from './components/ClassCounterOne'
+//import ClassCounterOne from './components/ClassCounterOne'
+//import HookCounterOne from './components/HookCounterOne'
 //import HookCounterFour from './components/HookCounterFour'
 //import ClassCounterTwo from './components/ClassCounterTwo'
 //import HookCounterThree from './components/HookCounterThree'
 //import ClassCounter from './components/ClassCounter'
 //import HookCounter from './components/HookCounter'
 //import HookCounterTwo from './components/HookCounterTwo'
+import ClassCounterUE from './components/ClassCounterUE'
 
 function App() {
   return (
     <div className="App">
-      <ClassCounterOne />
+      <ClassCounterUE />
     </div>
     
   )
